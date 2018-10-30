f08bf64404426ff1fd36123283c3a6ac7535fe7a
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-10-30 08:44:46 -0600

    Complete setup for M3 assessment.

--- app/models/play.rb
+++ app/models/play.rb
@@ -10,10 +10,17 @@ class Play < ApplicationRecord
       "K"=>5, "L"=>1, "M"=>3, "N"=>1, "O"=>1, "P"=>3, "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
       "U"=>1, "V"=>4, "W"=>4, "X"=>8, "Y"=>4, "Z"=>10
     }
+    # binding.pry
   end
 
   private
 
     def score_word
+    self.score =
+    word.upcase.chars.map do |letter|
+        letter_scores[letter]
+      end.sum
     end
+
+
 end
--- spec/models/play_spec.rb
+++ spec/models/play_spec.rb
@@ -5,6 +5,9 @@ describe Play do
     context "#score_word" do
       it "scores the word" do
         play = create(:play, word: "assess")
+        # # binding.pry
+        # play.letter_scores
+        # binding.pry
         expect(play.score).to eq(6)
       end
     end
