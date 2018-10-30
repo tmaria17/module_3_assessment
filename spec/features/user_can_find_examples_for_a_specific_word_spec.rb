85430fbdf0df1b9066b8a2f53aeb78ec3ff6899b
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2018-10-30 09:30:22 -0600

    basic feature test written, needs to be refactored to test for region.

---
+++ spec/features/user_can_find_examples_for_a_specific_word_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'user searches for a word' do
+  it 'displays example sentences' do
+#     As a guest user (no sign in required)
+# When I
+visit "/"
+fill_in :word, with: "mindfulness"
+click_on "Submit"
+expect(page).to have_content("Examples for using 'mindfulness'")
+expect(page).to have_content(sentences)
+expect(page).to have_css(".sentence", count: 2)
+# And I fill in a text box with "mindfulness"
+# And I click "Submit"
+# Then I should see a message that says "Examples for using 'mindfulness'"
+# And I should see a list of sentences with examples of how to use the word
+# And I should see only sentences where the region for usage is "Brittish" or "Canadian"
+# And I should not see sentences for any other regions (or blank regions)
+  end
+end
